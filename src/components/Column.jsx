7a33246e06cd51ad22b5bb3ea5aae8aa7c365932
Danilo Mendes <danilo.mirandha@gmail.com>
2024-07-06 13:17:06 -0300

    feat: add drag and drop tasks

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -2,6 +2,7 @@ import { shuffle } from "lodash";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Task from "./Task";
+import boardsSlice from "../redux/boardsSlice";
 
 const Column = ({ colIndex }) => {
   const colors = [
@@ -27,14 +28,34 @@ const Column = ({ colIndex }) => {
     setColor(shuffle(colors).pop());
   }, [dispatch]);
 
+  const handleOnDragOver = (e) => {
+    e.preventDefault();
+  };
+
+  const handleOnDrop = (e) => {
+    const { prevColIndex, taskIndex } = JSON.parse(
+      e.dataTransfer.getData("text")
+    );
+
+    if (colIndex !== prevColIndex) {
+      dispatch(
+        boardsSlice.actions.dragTask({ colIndex, prevColIndex, taskIndex })
+      );
+    }
+  };
+
   return (
-    <div className="scrollbar-hide mx-5 pt-[90px] min-w-[280px]">
+    <div
+      onDrop={handleOnDrop}
+      onDragOver={handleOnDragOver}
+      className="scrollbar-hide mx-5 pt-[90px] min-w-[280px]"
+    >
       <p className="font-semibold flex items-center gap-2 tracking-widest md:tracking-[.2em] text-[#828fa3]">
         <div className={`rounded-full w-4 h-4 ${color}`} />
-        {col.name} ({col.tasks.length})
+        {col.name} ({col?.tasks?.length})
       </p>
 
-      {col.tasks.map((task, index) => (
+      {col.tasks?.map((task, index) => (
         <Task key={index} taskIndex={index} colIndex={colIndex} />
       ))}
     </div>
